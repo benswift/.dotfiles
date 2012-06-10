e7f47adaf791510b53aa51b8375daf3822754776
Ben Swift <benjamin.j.swift@gmail.com>
2012-06-10 11:27:15 +1000

    monokai theme now treated as safe

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,1 +1,13 @@
 ;; customisations
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
