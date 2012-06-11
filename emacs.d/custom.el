06dd679a6904b29f5122bec6df1bffdc80a29628
Ben Swift <benjamin.j.swift@gmail.com>
2012-06-12 09:40:56 +1000

    rearranges the colours for outline (and org) mode headings to make them a bit nicer

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -10,4 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(outline-3 ((t (:inherit font-lock-type-face))))
+ '(outline-4 ((t (:inherit font-lock-string-face))))
+ '(outline-5 ((t (:inherit font-lock-constant-face))))
+ '(outline-6 ((t (:inherit font-lock-comment-face)))))
