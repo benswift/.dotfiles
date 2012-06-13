0b1a0ef13d568091a88a8f66b00044b35cf952d9
Ben Swift <benjamin.j.swift@gmail.com>
2012-06-13 18:22:08 +1000

    adds comment-dwim-line

--- emacs.d/ben.el
+++ emacs.d/ben.el
@@ -92,7 +92,6 @@
 
 ;; Mac OS X-like
 
-(global-set-key (kbd "s-'") 'comment-or-uncomment-region)
 (global-set-key (kbd "s-[") (lambda ()
                               (interactive)
                               (other-window -1)))
@@ -383,3 +382,17 @@
         (let ((face (or (get-char-property (point) 'read-face-name)
             (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
+;; from http://www.emacswiki.org/emacs/CommentingCode
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the `comment-dwim' command.
+If no region is selected and current line is not blank and we are not at the end of the line,
+then comment current line.
+Replaces default behaviour of `comment-dwim', when it inserts comment at the end of the line."
+    (interactive "*P")
+    (comment-normalize-vars)
+    (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+        (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+      (comment-dwim arg)))
+
+(global-set-key (kbd "s-'") 'comment-dwim-line)
