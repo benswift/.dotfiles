44ff2dbe9b66e5cedf630c4f8c8f51f6078ac358
Ben Swift <benjamin.j.swift@gmail.com>
2012-06-25 15:47:11 +1000

    some face colour changes for ERC mode

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -10,6 +10,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(erc-input-face ((t (:inherit font-lock-string-face))))
+ '(erc-my-nick-face ((t (:inherit font-lock-keyword-face :weight bold))))
+ '(erc-notice-face ((t (:inherit font-lock-warning-face))))
  '(outline-3 ((t (:inherit font-lock-type-face))))
  '(outline-4 ((t (:inherit font-lock-string-face))))
  '(outline-5 ((t (:inherit font-lock-constant-face))))
