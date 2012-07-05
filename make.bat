8d5a93bbb2f0866cba8b80c8e89584bfba6450fb
Ben Swift <benjamin.j.swift@gmail.com>
2012-07-05 17:16:26 +1000

    adds make.bat for symlinking on Windows
    must have the elevate and git-bash GNU command line tools installed

---
+++ make.bat
@@ -0,0 +1,11 @@
+elevate ln -sf "%HOME%\.dotfiles\zshrc" "%HOME%\.zshrc"
+elevate ln -sf "%HOME%\.dotfiles\exports" "%HOME%\.exports"
+elevate ln -sf "%HOME%\.dotfiles\aliases" "%HOME%\.aliases"
+
+elevate ln -sf "%HOME%\.dotfiles\gitconfig" "%HOME%\.gitconfig"
+elevate ln -sf "%HOME%\.dotfiles\gitignore" "%HOME%\.gitignore"
+
+elevate ln -sf "%HOME%\.dotfiles\emacs.d" "%HOME%\.emacs.d"
+
+elevate ln -sf "%HOME%\.dotfiles\RProfile" "%HOME%\.RProfile"
+elevate ln -sf "%HOME%\.dotfiles\ssh_config" "%HOME%\.ssh\config"
