6ac4122b97d5cda6f055c208ecd8f8c10884a7a4
Ben Swift <benjamin.j.swift@gmail.com>
2012-11-20 16:40:46 +1100

    change HOME -> HOMEPATH in make.bat

--- make.bat
+++ make.bat
@@ -1,11 +1,11 @@
-ln -sf "%HOME%\.dotfiles\zshrc" "%HOME%\.zshrc"
-ln -sf "%HOME%\.dotfiles\exports" "%HOME%\.exports"
-ln -sf "%HOME%\.dotfiles\aliases" "%HOME%\.aliases"
+ln -sf "%HOMEPATH%\.dotfiles\profile" "%HOMEPATH%\.profile"
+ln -sf "%HOMEPATH%\.dotfiles\exports" "%HOMEPATH%\.exports"
+ln -sf "%HOMEPATH%\.dotfiles\aliases" "%HOMEPATH%\.aliases"
 
-ln -sf "%HOME%\.dotfiles\gitconfig" "%HOME%\.gitconfig"
-ln -sf "%HOME%\.dotfiles\gitignore" "%HOME%\.gitignore"
+ln -sf "%HOMEPATH%\.dotfiles\gitconfig" "%HOMEPATH%\.gitconfig"
+ln -sf "%HOMEPATH%\.dotfiles\gitignore" "%HOMEPATH%\.gitignore"
 
-ln -sf "%HOME%\.dotfiles\emacs" "%HOME%\.emacs"
+ln -sf "%HOMEPATH%\.dotfiles\emacs" "%HOMEPATH%\.emacs"
 
-ln -sf "%HOME%\.dotfiles\RProfile" "%HOME%\.RProfile"
-ln -sf "%HOME%\.dotfiles\ssh_config" "%HOME%\.ssh\config"
+ln -sf "%HOMEPATH%\.dotfiles\RProfile" "%HOMEPATH%\.RProfile"
+ln -sf "%HOMEPATH%\.dotfiles\ssh_config" "%HOMEPATH%\.ssh\config"
