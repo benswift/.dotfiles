2afa5a690e49fa413af7b46ed4168b43d3ca2c01
Ben Swift <benjamin.j.swift@gmail.com>
2014-04-01 15:39:54 +1100

    add profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [cider/cider-nrepl "0.1.0-SNAPSHOT"]
+                  [lein-ancient "0.5.5"]]
+        :dependencies [[slamhound "1.5.3"]]}}
