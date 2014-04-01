94299e249571783e828a447c7ab9d02e7627839e
Ben Swift <benjamin.j.swift@gmail.com>
2014-04-01 16:03:44 +1100

    add alembic to lein dev plugins

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [cider/cider-nrepl "0.1.0-SNAPSHOT"]
                   [lein-ancient "0.5.5"]]
-        :dependencies [[slamhound "1.5.3"]]}}
+        :dependencies [[slamhound "1.5.3"]
+                       [alembic "0.2.1"]]}}
