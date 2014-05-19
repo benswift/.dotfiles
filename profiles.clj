ebb322c43653d0cf369eb1e5e74fd77214ab2efa
Ben Swift <benjamin.j.swift@gmail.com>
2014-05-19 17:29:21 +1000

    update lein plugins

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [cider/cider-nrepl "0.1.0-SNAPSHOT"]
+{:user {:plugins [[lein-cprint "1.0.0"]
+                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
                   [lein-ancient "0.5.5"]]
         :dependencies [[slamhound "1.5.3"]
                        [alembic "0.2.1"]]}}
