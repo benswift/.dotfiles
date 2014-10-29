e5f58c231c8eaff184a1e12c4b92373617d4ddcd
Ben Swift <benjamin.j.swift@gmail.com>
2014-10-29 19:13:06 +1100

    chain rsync calls with &&

--- scripts/simstim-backup.sh
+++ scripts/simstim-backup.sh
@@ -10,18 +10,18 @@ BACKUP_DIR=/home/ben/hodgey-backup/
 echo "Backing up to simstim: $SIMSTIM"
 
 # echo -e "\nBacking up \033[0;32mMusic\033[0;00m..."
-# rsync -avz -e ssh --delete --stats --human-readable --exclude='Music/iTunes/iTunes\ Media/Mobile\ Applications/' ~/Music ben@$SIMSTIM:$BACKUP_DIR/Music/
+# rsync -avz -e ssh --delete --stats --human-readable --exclude='Music/iTunes/iTunes\ Media/Mobile\ Applications/' ~/Music ben@$SIMSTIM:$BACKUP_DIR/Music/ && \
 # echo -e "\nBacking up \033[0;32mLibrary\033[0;00m..."
-# rsync -avz -e ssh --delete --stats --human-readable ~/Library ben@$SIMSTIM:$BACKUP_DIR/Library/
+# rsync -avz -e ssh --delete --stats --human-readable ~/Library ben@$SIMSTIM:$BACKUP_DIR/Library/ && \
 echo -e "\nBacking up \033[0;32mCalibre\033[0;00m..."
-rsync -avz -e ssh --delete --stats --human-readable ~/Calibre\ Library ben@$SIMSTIM:$BACKUP_DIR/Calibre\ Library/
+rsync -avz -e ssh --delete --stats --human-readable ~/Calibre\ Library ben@$SIMSTIM:$BACKUP_DIR/Calibre\ Library/ && \
 echo -e "\nBacking up \033[0;32mCode\033[0;00m..."
-rsync -avz -e ssh --delete --stats --human-readable ~/Code ben@$SIMSTIM:$BACKUP_DIR/Code/
+rsync -avz -e ssh --delete --stats --human-readable ~/Code ben@$SIMSTIM:$BACKUP_DIR/Code/ && \
 echo -e "\nBacking up \033[0;32mDesktop\033[0;00m..."
-# rsync -avz -e ssh --delete --stats --human-readable ~/Desktop ben@$SIMSTIM:$BACKUP_DIR/Desktop/
+# rsync -avz -e ssh --delete --stats --human-readable ~/Desktop ben@$SIMSTIM:$BACKUP_DIR/Desktop/ && \
 echo -e "\nBacking up \033[0;32mDocuments\033[0;00m..."
-rsync -avz -e ssh --delete --stats --human-readable ~/Documents ben@$SIMSTIM:$BACKUP_DIR/Documents/
+rsync -avz -e ssh --delete --stats --human-readable ~/Documents ben@$SIMSTIM:$BACKUP_DIR/Documents/ && \
 echo -e "\nBacking up \033[0;32mMovies\033[0;00m..."
-rsync -avz -e ssh --delete --stats --human-readable ~/Movies ben@$SIMSTIM:$BACKUP_DIR/Movies/
+rsync -avz -e ssh --delete --stats --human-readable ~/Movies ben@$SIMSTIM:$BACKUP_DIR/Movies/ && \
 echo -e "\nBacking up \033[0;32mPictures\033[0;00m..."
-rsync -avz -e ssh --delete --stats --human-readable ~/Pictures ben@$SIMSTIM:$BACKUP_DIR/Pictures/
+rsync -avz -e ssh --delete --stats --human-readable ~/Pictures ben@$SIMSTIM:$BACKUP_DIR/Pictures/ && \
