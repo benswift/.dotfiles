ae15fec2f14e0128ab61139dd985dd9610d4af78
Ben Swift <ben@benswift.me>
2015-01-20 19:56:51 +1100

    update clojure profile packages

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-cprint "1.0.0"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
-                  [lein-ancient "0.5.5"]]
-        :dependencies [[slamhound "1.5.3"]
-                       [alembic "0.2.1"]]}}
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [lein-ancient "0.6.0"]]
+        :dependencies [[slamhound "1.5.5"]
+                       [alembic "0.3.2"]]}}
