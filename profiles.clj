816ffc7378f02f34abab869ae0d2a29a9b0f89ab
Ben Swift <ben@benswift.me>
2015-01-21 15:07:54 +1100

    remove some unneeded clojure plugins

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,3 @@
-{:user {:plugins [[lein-cprint "1.0.0"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-ancient "0.6.0"]]
-        :dependencies [[slamhound "1.5.5"]
-                       [alembic "0.3.2"]]}}
+        :dependencies [[slamhound "1.5.5"]]}}
