209566292bc5488af7441b44ed457701ced559eb
Ben Swift <ben@benswift.me>
2015-01-28 13:24:58 +1100

    update make.bat

--- make.bat
+++ make.bat
@@ -1,11 +1,8 @@
-ln -sf "%HOMEPATH%\.dotfiles\profile" "%HOMEPATH%\.profile"
-ln -sf "%HOMEPATH%\.dotfiles\exports" "%HOMEPATH%\.exports"
-ln -sf "%HOMEPATH%\.dotfiles\aliases" "%HOMEPATH%\.aliases"
+mklink "C:%HOMEPATH%\.gitconfig" "C:%HOMEPATH%\.dotfiles\gitconfig"
+mklink "C:%HOMEPATH%\.gitignore" "C:%HOMEPATH%\.dotfiles\gitignore"
 
-ln -sf "%HOMEPATH%\.dotfiles\gitconfig" "%HOMEPATH%\.gitconfig"
-ln -sf "%HOMEPATH%\.dotfiles\gitignore" "%HOMEPATH%\.gitignore"
+mklink "C:%HOMEPATH%\.emacs" "C:%HOMEPATH%\.dotfiles\emacs"
+mklink /D "C:%HOMEPATH%\.emacs.d\snippets" "C:%HOMEPATH%\.dotfiles\snippets"
 
-ln -sf "%HOMEPATH%\.dotfiles\emacs" "%HOMEPATH%\.emacs"
+mklink "C:%HOMEPATH%\.RProfile" "C:%HOMEPATH%\.dotfiles\RProfile"
 
-ln -sf "%HOMEPATH%\.dotfiles\RProfile" "%HOMEPATH%\.RProfile"
-ln -sf "%HOMEPATH%\.dotfiles\ssh_config" "%HOMEPATH%\.ssh\config"
