a5eeb47695c9e7501def6a21f084997432fc45c7
Ben Swift <ben@benswift.me>
2015-01-29 11:10:34 +1100

    update make.bat for .emacs -> init.el change

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.dotfiles\gitconfig"
 mklink "%USERPROFILE%\.gitignore" "%USERPROFILE%\.dotfiles\gitignore"
 
-mklink "%USERPROFILE%\.emacs" "%USERPROFILE%\.dotfiles\emacs"
+mklink "%USERPROFILE%\.emacs.d\init.el" "%USERPROFILE%\.dotfiles\init.el"
 mklink /D "%USERPROFILE%\.emacs.d\snippets" "%USERPROFILE%\.dotfiles\snippets"
 
 mklink "%USERPROFILE%\.RProfile" "%USERPROFILE%\.dotfiles\RProfile"
