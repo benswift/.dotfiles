ea6403944dbb0088c4e4059169c404d0af4d1a04
Ben Swift <ben@benswift.me>
2015-11-26 13:56:23 +1100

    remove ace-jump-mode config

--- init.el
+++ init.el
@@ -646,21 +646,6 @@ i.e. change right window to bottom, or change bottom window to right."
                     "company"
                     "yas")))
 
-;;;;;;;;;;;;;;;;;;;
-;; ace-jump-mode ;;
-;;;;;;;;;;;;;;;;;;;
-
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
-(autoload
-  'ace-jump-mode-pop-mark
-  "ace-jump-mode"
-  "Ace jump back:-)"
-  t)
-(eval-after-load "ace-jump-mode"
-  '(ace-jump-mode-enable-mark-sync))
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-
 ;;;;;;;;;;;;;
 ;; company ;;
 ;;;;;;;;;;;;;
