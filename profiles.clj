a39466cb6aa4ceb4d23e9fa743a38a537a4450a1
Ben Swift <ben@benswift.me>
2015-11-30 17:35:54 +1100

    bugfix for profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.0"]]
-        :dependencies [[slamhound "1.5.5"]]}}
+{:user {:plugins [[lein-ancient "0.6.0"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
