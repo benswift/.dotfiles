03d012e6403a1847cf7d1d754702a9faa1c5ceed
Ben Swift <ben@benswift.me>
2016-01-11 13:59:58 +1100

    redefine some abo-abo keybindings

--- init.el
+++ init.el
@@ -207,13 +207,9 @@
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
 (global-set-key (kbd "<f1> l") 'counsel-load-library)
 (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
 (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
@@ -225,7 +221,9 @@
 (avy-setup-default)
 (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "C-'") 'avy-goto-char-2)
-(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "C-;") 'avy-goto-word-1)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; garbage collection ;;
