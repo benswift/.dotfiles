b7e07b0b5a2aafd62177b3e9be9e7d62f4c82d9c
Ben Swift <ben@benswift.me>
2016-01-12 15:00:21 +1100

    remove unused keybindings

--- init.el
+++ init.el
@@ -196,8 +196,6 @@
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> l") 'counsel-load-library)
@@ -205,7 +203,6 @@
 (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 
 ;;;;;;;;;
 ;; avy ;;
