82cff67db994763317ff92397b8a0c5e03fb6a05
Ben Swift <ben@benswift.me>
2016-02-10 10:12:16 +1100

    update lein plugins

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[lein-ancient "0.6.0"]]}
+{:user {:plugins [[lein-ancient "0.6.8"]]}
  :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
