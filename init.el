c4f781c4730e7124e7cb868c0fb6fa1eefc30a4a
Ben Swift <ben@benswift.me>
2016-07-15 12:25:03 +1000

    change swiper to counsel-grep-or-swiper

--- init.el
+++ init.el
@@ -199,7 +199,7 @@
 (setq ivy-height 20)
 (setq ivy-use-virtual-buffers t)
 (ivy-mode 1)
-(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-s") 'counsel-grep-or-swiper)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> l") 'counsel-load-library)
