acd82308dce0126c901746a5d59e1bb96ec274ef
Ben Swift <ben@benswift.me>
2017-10-05 11:22:05 +1100

    update windows dotfile make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,3 @@
 mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.dotfiles\gitconfig"
 mklink "%USERPROFILE%\.gitignore" "%USERPROFILE%\.dotfiles\gitignore"
-
-mklink "%USERPROFILE%\.emacs.d\init.el" "%USERPROFILE%\.dotfiles\init.el"
-mklink /D "%USERPROFILE%\.emacs.d\snippets" "%USERPROFILE%\.dotfiles\snippets"
-
-mklink "%USERPROFILE%\.RProfile" "%USERPROFILE%\.dotfiles\RProfile"
+mklink "%USERPROFILE%\.spacemacs" "%USERPROFILE%\.dotfiles\spacemacs"
