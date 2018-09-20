207701da1d9daaf3d70f027324e2463ef287a633
Ben Swift <ben@benswift.me>
2018-09-20 13:57:18 +1000

    make varese video upload script take filename args

--- scripts/comp1720-varese-upload.sh
+++ scripts/comp1720-varese-upload.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scp comp1720-*.mp4 comp1720@varese.anu.edu.au:/courses/comp1720/public_html/lecture-recordings
+scp $@ comp1720@varese.anu.edu.au:/courses/comp1720/public_html/lecture-recordings
