22f1b5e52e0aee9f86d8ae0f3a66db290d6eff97
Ben Swift <ben@benswift.me>
2018-12-15 10:00:53 +1100

    tweak header for spacemacs layer file

--- spacemacs-layers/extempore/packages.el
+++ spacemacs-layers/extempore/packages.el
@@ -1,31 +1,15 @@
-;;; packages.el --- extempore layer packages file for Spacemacs.
+;;; packages.el --- Extempore layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;; Copyright (c) 2017-2018 Ben Swift
 ;;
 ;; Author: Ben Swift <ben@benswift.me>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; License: GPLv3
+;;; License: MIT
 
 ;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `extempore-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `extempore/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `extempore/pre-init-PACKAGE' and/or
-;;   `extempore/post-init-PACKAGE' to customize the package as it is loaded.
 
 ;;; Code:
 
