000ca4b0f8a9fcf96b86002c5415c86b36da8b39
Ben Swift <ben@benswift.me>
2019-01-15 16:52:58 +1100

    duck script should use synchronize

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -2,4 +2,4 @@
 
 # duck -u ben.swift@anu.edu.au --list davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/
 
-duck -u ben.swift@anu.edu.au --parallel 8 -e compare --upload davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/smithy/ ~/Documents/
+duck -u ben.swift@anu.edu.au --assumeyes --parallel 8 --existing upload --synchronize davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/smithy/ ~/Documents/
