e44e0b763d1e592620b41bfcb88cafa84cb9a680
Ben Swift <ben@benswift.me>
2019-01-15 12:59:41 +1100

    add htmlproofer script

---
+++ scripts/htmlproofer.sh
@@ -0,0 +1,6 @@
+LANG=en_AU.UTF-8
+LANGUAGE=en_AU.UTF-8
+LC_ALL=en_AU.UTF-8
+
+# bundle exec jekyll build && htmlproofer --log-level ':debug' --assume-extension _site
+bundle exec jekyll build && htmlproofer --assume-extension _site
