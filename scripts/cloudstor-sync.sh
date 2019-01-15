05fc56554ab4d54e813e5d7547dbccd945fc0da0
Ben Swift <ben@benswift.me>
2019-01-15 16:39:05 +1100

    fix cloudstor-sync.sh script
    needed ben.swift, not u-number

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-duck -u u2548636@anu.edu.au --list davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/
+# duck -u ben.swift@anu.edu.au --list davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/
 
-# duck -u u2548636@anu.edu.au --parallel 8 -e compare --upload davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/smithy-documents/ ~/Documents/
+duck -u ben.swift@anu.edu.au --parallel 8 -e compare --upload davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/smithy-documents/ ~/Documents/
