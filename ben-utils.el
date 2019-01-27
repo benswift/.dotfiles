0a54430f540710c21eda5c5df8cbdbfa2bca3d9d
Ben Swift <ben@benswift.me>
2019-01-27 18:13:19 +1100

    add unfill-paragraph

--- ben-utils.el
+++ ben-utils.el
@@ -554,6 +554,15 @@ Version 2017-01-11"
 			   (replace-match (aref $x 1) "FIXEDCASE" "LITERAL")))
 		   $strPairs))))))
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 (require 'sgml-mode)
 
 (defun ben-reformat-xml ()
