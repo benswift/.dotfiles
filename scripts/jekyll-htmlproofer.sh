551735f2b3eb6b905a88de90bacd8fe611fb0416
Ben Swift <ben@benswift.me>
2019-01-31 14:12:15 +1100

    update htmlproofer invocation to not check the anu style domain
    because they bad citizens.

--- scripts/jekyll-htmlproofer.sh
+++ scripts/jekyll-htmlproofer.sh
@@ -1,2 +1,2 @@
 # bundle exec jekyll build && htmlproofer --log-level ':debug' --assume-extension _site
-bundle exec jekyll build && htmlproofer --assume-extension _site
+bundle exec jekyll build && htmlproofer --alt-ignore "/style.anu.edu.au/" --file-ignore "/404/" --assume-extension _site
