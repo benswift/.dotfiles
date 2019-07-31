c5256b7e136569e53670a3cb3eaae41b4e4d512e
Ben Swift <ben@benswift.me>
2019-07-31 14:09:03 +1000

    update lecture recording scp script

--- scripts/comp1720-varese-upload.sh
+++ scripts/comp1720-varese-upload.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-scp $@ comp1720@varese.anu.edu.au:/courses/comp1720/public_html/lecture-recordings/2019
+scp -i ~/.ssh/comp1720-varese $@ comp1720@varese.anu.edu.au:/courses/comp1720/public_html/lecture-recordings/2019
