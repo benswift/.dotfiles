a349dfd95716d5c608cb142fe0c8ecac85aafa6c
Ben Swift <ben@benswift.me>
2020-04-09 09:29:36 +1000

    better header for ben-utils.el

--- ben-utils.el
+++ ben-utils.el
@@ -6,25 +6,16 @@
 ;; Package-Requires: (csv url-util mu4e-contrib smtpmail smtpmail-async gnus-dired)
 ;; Homepage: https://github.com/benswift/.dotfiles
 
-;; Copyright 2020 Ben Swift
+;;; ben-utils.el --- Ben's elisp utilities; the source of his powers
 
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
+;; Copyright (c) 2017-2020 Ben Swift
+;;
+;; Author: Ben Swift <ben@benswift.me>
+;; URL: https://github.com/benswift/.dotfiles/blob/master/ben-utils.el
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: MIT
 
 ;;; Commentary:
 
