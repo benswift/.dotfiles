a7eb800191dca81934d9cd54ab0164db3fc9858d
Ben Swift <ben@benswift.me>
2020-04-22 20:01:24 +1000

    remove the w-e-a-l wrapper around the Extempore config
    it wasn't working, will fix later

--- ben-utils.el
+++ ben-utils.el
@@ -369,101 +369,100 @@ nothing"
 ;; Extempore ;;
 ;;;;;;;;;;;;;;;
 
-(with-eval-after-load 'extempore
-  (cond
-   ((string= (system-name) "paranoid-android")
-	(setq extempore-path "/home/ben/extempore/")
-	(setq user-extempore-lib-directory "/home/ben/Code/xtm/lib/"))
-   ((string= "smithy" (system-name))
-	(setq extempore-program-args "--frames 4096")
-	(setq extempore-path "~/Documents/research/extemporelang/extempore/")
-	(setq user-extempore-lib-directory "~/Documents/research/extemporelang/xtm/"))
-   (t
-	(message "unrecognised machine (%s), skipping Extempore mode var setup" (system-name))))
+(cond
+ ((string= (system-name) "paranoid-android")
+  (setq extempore-path "/home/ben/extempore/")
+  (setq user-extempore-lib-directory "/home/ben/Code/xtm/lib/"))
+ ((string= "smithy" (system-name))
+  (setq extempore-program-args "--frames 4096")
+  (setq extempore-path "~/Documents/research/extemporelang/extempore/")
+  (setq user-extempore-lib-directory "~/Documents/research/extemporelang/xtm/"))
+ (t
+  (message "unrecognised machine (%s), skipping Extempore mode var setup" (system-name))))
 
-  (defun extempore-create-template-file (base-path filename &optional header)
-	(let ((full-path (format "%s/%s" base-path filename)))
-	  (unless (file-exists-p full-path)
-		(progn
-		  (find-file full-path)
-		  (if header (insert header))
-		  (save-buffer)
-		  (kill-buffer)))))
+(defun extempore-create-template-file (base-path filename &optional header)
+  (let ((full-path (format "%s/%s" base-path filename)))
+	(unless (file-exists-p full-path)
+	  (progn
+		(find-file full-path)
+		(if header (insert header))
+		(save-buffer)
+		(kill-buffer)))))
 
-  (defun extempore-create-template (name)
-	"Set up the directory structure and files for a new extempore session/gig."
-	(interactive "sSession name: ")
-	(let* ((xtm-dir (expand-file-name "~/Documents/research/extemporelang/xtm/"))
-		   (base-path (concat xtm-dir "sessions/" name))
-		   (setup-header
-			(format ";;; setup.xtm --- setup file for %s
+(defun extempore-create-template (name)
+  "Set up the directory structure and files for a new extempore session/gig."
+  (interactive "sSession name: ")
+  (let* ((xtm-dir (expand-file-name "~/Documents/research/extemporelang/xtm/"))
+		 (base-path (concat xtm-dir "sessions/" name))
+		 (setup-header
+		  (format ";;; setup.xtm --- setup file for %s
 
 (sys:load \"%slib/benlib-scm.xtm\")
 
 dspmt" name xtm-dir)))
-	  (if (file-exists-p base-path)
-		  (error "Cannot create xtm session: directory already exists."))
-	  (make-directory base-path)
-	  (extempore-create-template-file
-	   base-path "practise.xtm" "headerp")
-	  (extempore-create-template-file
-	   base-path "gig.xtm" "headerp")
-	  (extempore-create-template-file
-	   base-path "setup.xtm" setup-header)
-	  (dired base-path)))
+	(if (file-exists-p base-path)
+		(error "Cannot create xtm session: directory already exists."))
+	(make-directory base-path)
+	(extempore-create-template-file
+	 base-path "practise.xtm" "headerp")
+	(extempore-create-template-file
+	 base-path "gig.xtm" "headerp")
+	(extempore-create-template-file
+	 base-path "setup.xtm" setup-header)
+	(dired base-path)))
 
-  ;; llvm-emacs utils
+;; llvm-emacs utils
 
-  (setq load-path
-		(cons (expand-file-name "~/.dotfiles/llvm-emacs") load-path))
+(setq load-path
+	  (cons (expand-file-name "~/.dotfiles/llvm-emacs") load-path))
 
-  (require 'llvm-mode)
-  (require 'tablegen-mode)
+(require 'llvm-mode)
+(require 'tablegen-mode)
 
-  (defun extempore-show-ir-in-temp-buffer (beg end)
-	(interactive "r")
-	(save-excursion
-	  (let ((ir-str (buffer-substring-no-properties beg end)))
-		(with-current-buffer (get-buffer-create "*extempore LLVM IR*")
-		  (if (not (equal major-mode 'llvm-mode))
-			  (llvm-mode))
-		  (delete-region (point-min) (point-max))
-		  (insert (replace-regexp-in-string "\\\\n" "\n" ir-str))
-		  (display-buffer "*extempore LLVM IR*" #'display-buffer-pop-up-window)))))
+(defun extempore-show-ir-in-temp-buffer (beg end)
+  (interactive "r")
+  (save-excursion
+	(let ((ir-str (buffer-substring-no-properties beg end)))
+	  (with-current-buffer (get-buffer-create "*extempore LLVM IR*")
+		(if (not (equal major-mode 'llvm-mode))
+			(llvm-mode))
+		(delete-region (point-min) (point-max))
+		(insert (replace-regexp-in-string "\\\\n" "\n" ir-str))
+		(display-buffer "*extempore LLVM IR*" #'display-buffer-pop-up-window)))))
 
-  ;; Ben's livecoding snippet helpers
+;; Ben's livecoding snippet helpers
 
-  ;; used in extempore-mode's print-line-debug snippet
-  (defun extempore-yas-println-debug-expander (pl-str format-str)
-	(if (not (string= pl-str ""))
-		(mapconcat (lambda (name) (format format-str name name))
-				   (cl-remove-if (lambda (x) (or (string-match "^'.*:$" x)
-												 (string-match "^\".*:\"$" x)))
-								 (split-string pl-str " "))
-				   " ")
-	  pl-str))
+;; used in extempore-mode's print-line-debug snippet
+(defun extempore-yas-println-debug-expander (pl-str format-str)
+  (if (not (string= pl-str ""))
+	  (mapconcat (lambda (name) (format format-str name name))
+				 (cl-remove-if (lambda (x) (or (string-match "^'.*:$" x)
+											   (string-match "^\".*:\"$" x)))
+							   (split-string pl-str " "))
+				 " ")
+	pl-str))
 
-  (defvar extempore-yas-oscillator-list '("osc" "square" "triangle" "rect" "saw" "pulse" "fade" "delay" "delay_t" "comb" "flanger" "chorus" "tap_delay" "allpass" "reverb" "reverb2" "hold" "svf" "lpf" "lpf2" "bpf" "hpf" "notch" "peak" "lshelf" "hshelf" "skf" "lpfbq" "hpfbq" "bpfbq" "notchbq" "vcf" "hann" "hann_t" "linear"))
+(defvar extempore-yas-oscillator-list '("osc" "square" "triangle" "rect" "saw" "pulse" "fade" "delay" "delay_t" "comb" "flanger" "chorus" "tap_delay" "allpass" "reverb" "reverb2" "hold" "svf" "lpf" "lpf2" "bpf" "hpf" "notch" "peak" "lshelf" "hshelf" "skf" "lpfbq" "hpfbq" "bpfbq" "notchbq" "vcf" "hann" "hann_t" "linear"))
 
-  (defun extempore-yas-get-sample-map-list ()
-	(if (boundp 'user-extempore-lib-directory)
-		(with-temp-buffer
-		  (insert-file-contents (concat user-extempore-lib-directory "sampler-maps.xtm"))
-		  (goto-char (point-min))
-		  (cl-labels ((sm-parse-fn (sm-list)
-								   (if (re-search-forward "(define \\(*sm-[^ \n]*\\)" nil :no-error)
-									   (funcall #'sm-parse-fn (cons (match-string-no-properties 1) sm-list))
-									 sm-list)))
-			(sm-parse-fn nil)))
-	  '("")))
+(defun extempore-yas-get-sample-map-list ()
+  (if (boundp 'user-extempore-lib-directory)
+	  (with-temp-buffer
+		(insert-file-contents (concat user-extempore-lib-directory "sampler-maps.xtm"))
+		(goto-char (point-min))
+		(cl-labels ((sm-parse-fn (sm-list)
+								 (if (re-search-forward "(define \\(*sm-[^ \n]*\\)" nil :no-error)
+									 (funcall #'sm-parse-fn (cons (match-string-no-properties 1) sm-list))
+								   sm-list)))
+		  (sm-parse-fn nil)))
+	'("")))
 
-  (defun extempore-yas-get-chord-sym (maj-min)
-	;; symbol lists from libs/core/pc_ivl.xtm
-	(mapcar #'symbol-name
-			(case maj-min
-			  ('^ 5 '(i i6 i64 i7 i- i-7 n n6 ii ii6 ii7 ii9 ii^ ii^7 iii iii6 iii7 iii^ iii^7 iv iv6 iv7 iv- iv-7 v v6 v7 v- v-7 vi vi6 vi7 vi^ vi^7 viio viio7 vii vii7))
-			  ('- '(i i6 i64 i7 i^ i^6 i^64 i^7 n n6 ii ii6 ii7 ii- ii-6 ii-7 ii^ ii^7 iii iii6 iii7 iii- iii-6 iii-7 iv iv6 iv7 iv^ iv^6 iv^7 v v^ v6 v7 v- v-6 v-6 v-7 vi vi6 vi7 vi- vi-6 vi-7 vii vii6 vii7 viio viio6 viio7))
-			  (t nil)))))
+(defun extempore-yas-get-chord-sym (maj-min)
+  ;; symbol lists from libs/core/pc_ivl.xtm
+  (mapcar #'symbol-name
+		  (case maj-min
+			('^ 5 '(i i6 i64 i7 i- i-7 n n6 ii ii6 ii7 ii9 ii^ ii^7 iii iii6 iii7 iii^ iii^7 iv iv6 iv7 iv- iv-7 v v6 v7 v- v-7 vi vi6 vi7 vi^ vi^7 viio viio7 vii vii7))
+			('- '(i i6 i64 i7 i^ i^6 i^64 i^7 n n6 ii ii6 ii7 ii- ii-6 ii-7 ii^ ii^7 iii iii6 iii7 iii- iii-6 iii-7 iv iv6 iv7 iv^ iv^6 iv^7 v v^ v6 v7 v- v-6 v-6 v-7 vi vi6 vi7 vi- vi-6 vi-7 vii vii6 vii7 viio viio6 viio7))
+			(t nil))))
 
 ;;;;;;;;;;;;;;
 ;; org-mode ;;
