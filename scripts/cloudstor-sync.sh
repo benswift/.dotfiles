040168445d4e402b2b8cda6a3c5b3ef42888d654
Ben Swift <ben@benswift.me>
2020-05-27 11:43:17 +1000

    switch cloudstor-sync.sh to use rclone
    so much better.

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# to list what's there:
-# duck -u ben.swift@anu.edu.au --list davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/
-
-declare -a FOLDERS=("Calibre Library" "Zotero" "business" "church" "misc" "personal" "research" "teaching")
-
-# Read the array values with space
-for folder in "${FOLDERS[@]}"; do
-	duck -u ben.swift@anu.edu.au\
-		 --assumeyes\
-		 --quiet\
-		 --parallel 4\
-		 --existing compare\
-		 --upload davs://cloudstor.aarnet.edu.au/plus/remote.php/webdav/smithy-duck/Documents/\
-		 "~/Documents/$folder"
-done
+rclone sync --progress ~/Documents cloudstor:smithy-rclone/Documents
