18a108013be27f4cfb5e233a9b3b568c9e61a8e3
Ben Swift <ben@benswift.me>
2020-06-28 16:38:40 +1000

    using the gem directly as a build hook is better

--- scripts/jekyll-htmlproofer.sh
+++
@@ -1,2 +0,0 @@
-# bundle exec jekyll build && htmlproofer --log-level ':debug' --assume-extension _site
-bundle exec jekyll build && htmlproofer --alt-ignore "/style.anu.edu.au/" --file-ignore "/404/" --assume-extension _site
