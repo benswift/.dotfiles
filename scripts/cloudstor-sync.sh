ce4cd0d3bdf5baf56b7761c9ee4508cf0e5cb314
Ben Swift <ben@benswift.me>
2020-07-17 11:37:24 +1000

    cloudstor-sync.sh: add an exclude file
    don't need to sync caches, .git/ folders, etc.

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rclone sync --progress ~/Documents cloudstor:smithy-rclone/Documents
+rclone sync --progress --exclude-from ~/.dotfiles/scripts/cloudstor-exclude-files.txt ~/Documents cloudstor:smithy-rclone/Documents
