a14ed99340879d3ffc5b495b0dd15d1dae3b6a5e
Ben Swift <ben@benswift.me>
2020-12-18 12:10:53 +0800

    add separate "sync org directory" thing to the cloudstor script
    (just to make that happen first)

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+rclone sync --progress --exclude /roam/db/ ~/Documents/org cloudstor:smithy-rclone/Documents/org
 rclone sync --progress --exclude-from ~/.dotfiles/scripts/cloudstor-exclude-files.txt ~/Documents cloudstor:smithy-rclone/Documents
