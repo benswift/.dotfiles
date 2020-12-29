bdfe151c03530756090805966398a616f1f502b5
Ben Swift <ben@benswift.me>
2020-12-29 14:03:46 +0800

    remove the org-directory special case from cloudstor-sync.sh
    because we've now got ben-sync-org-directory-to-github

--- scripts/cloudstor-sync.sh
+++ scripts/cloudstor-sync.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rclone sync --progress --exclude /roam/db/ ~/Documents/org cloudstor:smithy-rclone/Documents/org
 rclone sync --progress --exclude-from ~/.dotfiles/scripts/cloudstor-exclude-files.txt ~/Documents cloudstor:smithy-rclone/Documents
