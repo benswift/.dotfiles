21cae14db7cc0cb1fb97337cf22eaad2bbeab96e
Ben Swift <ben@benswift.me>
2021-09-06 13:20:09 +1000

    update vpn script to pull info out of gpg-encryped file
    with gpg-agent, this means not *quite* so many "input password" events

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,1 +1,7 @@
-openconnect --user=u2548636 --protocol=gp https://staff-access.anu.edu.au
+#!/usr/bin/env zsh
+
+# pull ANU password out of encrypted authinfo file, pipe it to stdin
+gpg -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | \
+    awk '/machine smtp.office365.com login u2548636@anu.edu.au/ {print $NF}' | \
+    # start the VPN
+    openconnect --user=u2548636 --protocol=gp --passwd-on-stdin https://staff-access.anu.edu.au
