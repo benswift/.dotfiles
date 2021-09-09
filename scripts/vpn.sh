d87df28117d15609f87ea37f299821cd9e5dade2
Ben Swift <ben@benswift.me>
2021-09-09 13:20:19 +1000

    only use sudo for openconnect, not the whole thing
    this is better, and gpg no longer gives a warning because it's running as a
    different user

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -4,4 +4,4 @@
 gpg -q --for-your-eyes-only --no-tty -d ~/.authinfo.gpg | \
     awk '/machine smtp.office365.com login u2548636@anu.edu.au/ {print $NF}' | \
     # start the VPN
-    openconnect --user=u2548636 --protocol=gp --passwd-on-stdin https://staff-access.anu.edu.au
+    sudo openconnect --user=u2548636 --protocol=gp --passwd-on-stdin https://staff-access.anu.edu.au
