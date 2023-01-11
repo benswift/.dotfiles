bb43fc33a84697eec122d16752dcaf7ba7cbc5a3
Ben Swift <ben@benswift.me>
2023-01-11 12:11:43 +1100

    set up rclone backup script for onedrive

--- scripts/cloudstor-sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rclone sync --progress --exclude-from ~/.dotfiles/scripts/cloudstor-exclude-files.txt ~/Documents cloudstor:mitch-rclone/Documents
---
+++ scripts/rclone-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+EXCLUDE_FROM_FILE="/tmp/rclone-excludes.txt"
+CLONE_ROOT_DIR=~/Documents
+REMOTE=anu-onedrive
+
+## find all git repo enclosing folders (including trailing slash), munge them
+## into the form that rclone expects for its "exclude from" file
+cd $CLONE_ROOT_DIR && find . -type d -name .git -exec  dirname {} \; | sed -e 's/$/\//' -e 's/^.//' > $EXCLUDE_FROM_FILE
+
+## add a few extra excludes
+echo ".DS_Store" >> $EXCLUDE_FROM_FILE
+
+## sync to remote
+rclone sync --progress --exclude-from=$EXCLUDE_FROM_FILE $CLONE_ROOT_DIR $REMOTE:mitch-rclone/$CLONE_ROOT_DIR
