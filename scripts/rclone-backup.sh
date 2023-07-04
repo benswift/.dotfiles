f3e72a8501825094547f63bc903cd01903da0c06
Ben Swift <ben@benswift.me>
2023-07-04 14:39:05 +1000

    add some "update" echos to the backup script

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -8,10 +8,13 @@ REMOTE=anu-onedrive
 
 ## find all git repo enclosing folders (including trailing slash), munge them
 ## into the form that rclone expects for its "exclude from" file
+echo "finding git repos to ignore..."
 cd $CLONE_ROOT_DIR && find . -type d -name .git -exec  dirname {} \; | sed -e 's/$/\//' -e 's/^.//' > $EXCLUDE_FROM_FILE
 
 ## add a few extra excludes
 echo ".DS_Store" >> $EXCLUDE_FROM_FILE
+echo "ignoring files in $(wc -l < $EXCLUDE_FROM_FILE | tr -s " ") git repos"
 
 ## sync to remote
+echo "syncing to remote $REMOTE"
 rclone sync --progress --exclude-from=$EXCLUDE_FROM_FILE $CLONE_ROOT_DIR $REMOTE:mitch-rclone/$CLONE_ROOT_DIR
