0fa80188df595a1c13a245457a9e8dba21b909c2
Ben Swift <ben@benswift.me>
2023-08-23 20:48:27 +1000

    doom: add lookup module

--- doom-init.el
+++ doom-init.el
@@ -95,7 +95,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       ;;lookup            ; navigate your code and its documentation
+       lookup              ; navigate your code and its documentation
        lsp                 ; M-x vscode
        magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
