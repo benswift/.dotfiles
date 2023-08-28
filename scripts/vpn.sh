9eeee4a3ba8eb9d0a0028e6f02c5902deeada997
Ben Swift <ben@benswift.me>
2023-08-28 10:27:13 +1000

    format shell script with shfmt
    not actually sure if it's valid... will check when I get around to it

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 # pull ANU password out of 1Password, pipe it to stdin
-op item get "ANU Identity" --fields pw |\
+op item get "ANU Identity" --fields pw |
     # start the VPN
     sudo openconnect --user=u2548636 --protocol=gp --passwd-on-stdin https://staff-access.anu.edu.au
