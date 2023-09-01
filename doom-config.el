d4815bb45d3708c83dd7aed367e7e5d6f85986a8
Ben Swift <ben@benswift.me>
2023-09-02 07:48:33 +1000

    switch to doom-one theme

--- doom-config.el
+++ doom-config.el
@@ -24,7 +24,7 @@
 (setq doom-font (font-spec :family "Operator Mono" :size 16 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16))
 
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-one)
 
 (setq display-line-numbers-type nil)
 
