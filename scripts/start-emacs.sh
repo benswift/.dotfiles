1f2e5730d1160a45659bd034b26757d6067f8ef2
Ben Swift <ben@benswift.me>
2023-09-02 07:48:43 +1000

    make start-emacs.sh run it in the background

--- scripts/start-emacs.sh
+++ scripts/start-emacs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # from https://github.com/railwaycat/homebrew-emacsmacport/blob/master/docs/emacs-start-helpers.md
-exec $(brew --prefix)/opt/emacs-mac/Emacs.app/Contents/MacOS/Emacs.sh "$@"
+exec $(brew --prefix)/opt/emacs-mac/Emacs.app/Contents/MacOS/Emacs.sh "$@" &
