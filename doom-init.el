d5a29161fcc5f030b68759822b19ef962e2a6ac3
Ben Swift <ben@benswift.me>
2023-09-06 16:55:07 +1000

    doom: add lsp for yaml

--- doom-init.el
+++ doom-init.el
@@ -174,7 +174,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +treesitter)   ; the tubes
-       (yaml +treesitter)  ; JSON, but readable
+       (yaml +lsp +treesitter)  ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
