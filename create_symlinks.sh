8802d27a128ae9d676f47d6a926f8e52d7a4601e
Ben Swift <ben@benswift.me>
2025-01-22 10:45:11 +1100

    move zed config into separate subfolder
    and update create_symlinks.sh to reflect this change

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Files to link
+# Files to link in home directory
 files=(
     "RProfile"
     "gitconfig"
@@ -9,13 +9,33 @@ files=(
     "zshrc"
 )
 
+# Zed config files to link
+zed_files=(
+    "keymap.json"
+    "settings.json"
+    "tasks.json"
+)
+
 # Get the absolute path of the .dotfiles directory
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# Create symlinks
+# Create symlinks in home directory
 for file in "${files[@]}"; do
     target="$HOME/.${file}"
     source_path="$DOTFILES_DIR/$file"
+
+    # Create symlink (overwriting if exists)
+    ln -sf "$source_path" "$target"
+    echo "Created symlink: $target -> $source_path"
+done
+
+# Create .config/zed directory if it doesn't exist
+mkdir -p "$HOME/.config/zed"
+
+# Create symlinks for zed config files
+for file in "${zed_files[@]}"; do
+    target="$HOME/.config/zed/${file}"
+    source_path="$DOTFILES_DIR/zed/${file}"
 
     # Create symlink (overwriting if exists)
     ln -sf "$source_path" "$target"
