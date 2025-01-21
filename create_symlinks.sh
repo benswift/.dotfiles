523c4a4115be7690334319a90bc336461e5a1cec
Ben Swift <ben@benswift.me>
2025-01-22 10:39:26 +1100

    simplify script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -17,12 +17,7 @@ for file in "${files[@]}"; do
     target="$HOME/.${file}"
     source_path="$DOTFILES_DIR/$file"
 
-    # Remove existing symlink or file
-    if [ -e "$target" ] || [ -L "$target" ]; then
-        rm "$target"
-    fi
-
-    # Create new symlink
-    ln -s "$source_path" "$target"
+    # Create symlink (overwriting if exists)
+    ln -sf "$source_path" "$target"
     echo "Created symlink: $target -> $source_path"
 done
