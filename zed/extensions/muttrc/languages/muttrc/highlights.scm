64cb75f66ddd2b8b651c2454389fc2843926d546
Ben Swift <ben@benswift.me>
2025-09-02 21:51:47 +1000

    muttrc colouring works, with some bugs

--- zed/extensions/muttrc/languages/muttrc/highlights.scm
+++ zed/extensions/muttrc/languages/muttrc/highlights.scm
@@ -1,110 +1,27 @@
 ; Comments
 (comment) @comment
 
-; Keywords
-[
-  "set"
-  "unset"
-  "reset"
-  "toggle"
-  "source"
-  "macro"
-  "bind"
-  "unbind"
-  "exec"
-  "push"
-  "color"
-  "uncolor"
-  "mono"
-  "unmono"
-  "alias"
-  "unalias"
-  "mailboxes"
-  "unmailboxes"
-  "subscribe"
-  "unsubscribe"
-  "lists"
-  "unlists"
-  "hdr_order"
-  "unhdr_order"
-  "auto_view"
-  "unauto_view"
-  "alternative_order"
-  "unalternative_order"
-  "ignore"
-  "unignore"
-  "score"
-  "unscore"
-  "folder-hook"
-  "send-hook"
-  "send2-hook"
-  "reply-hook"
-  "account-hook"
-  "pgp-hook"
-  "crypt-hook"
-  "message-hook"
-  "save-hook"
-  "fcc-hook"
-  "fcc-save-hook"
-  "charset-hook"
-  "iconv-hook"
-  "mbox-hook"
-  "tag-transforms"
-  "tag-formats"
-] @keyword
-
 ; String literals
 (string) @string
-(quoted_string) @string
-(backtick_expansion) @string.special
+(shell) @string.special
 
-; Variables
-(variable) @variable
-(my_var) @variable
-(shell_command) @function.call
+; Commands (the actual command word like "set", "bind", etc.)
+(command) @keyword
 
-; Numbers
-(number) @number
+; Options (variable names)
+(option) @variable
 
-; Boolean values
-[
-  "yes"
-  "no"
-  "ask-yes"
-  "ask-no"
-] @constant.builtin
+; Keys
+(key) @constant
 
-; Operators
-[
-  "="
-  "+="
-  "-="
-  "?="
-] @operator
+; Objects
+(object) @type
 
-; Special characters
-[
-  "+"
-  "-"
-  "*"
-  "/"
-  "%"
-  "!"
-  "~"
-  "^"
-] @punctuation.special
+; Colors
+(color) @constant.builtin
 
-; Delimiters
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-] @punctuation.bracket
+; Functions
+(function) @function
 
-[
-  ","
-  ";"
-] @punctuation.delimiter
+; Operators and punctuation
+"=" @operator
