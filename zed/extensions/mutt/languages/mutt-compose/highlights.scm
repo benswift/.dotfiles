0c7dae44929010f0712852fa8760725eb6841e8a
Ben Swift <ben@benswift.me>
2025-09-03 15:51:51 +1000

    Remove mutt Zed extension files
    Moved to separate repository at https://github.com/benswift/zed-mutt

--- zed/extensions/mutt/languages/mutt-compose/highlights.scm
+++
@@ -1,5 +0,0 @@
-; Email headers
-(header key: (_) @keyword)
-(header value: (_) @string)
-
-; The body will be highlighted by the injected markdown grammar
--- zed/extensions/mutt/languages/mutt-compose/injections.scm
+++
@@ -1,3 +0,0 @@
-; Inject markdown (GFM) grammar into the body
-((body) @injection.content
-  (#set! injection.language "markdown"))
