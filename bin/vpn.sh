47e3ef41d0ec7b6e4e6954a9bf6b1bbf382b3139
Ben Swift <ben@benswift.me>
2026-05-20 15:43:03 +1000

    vpn: use op read secret reference

--- bin/vpn.sh
+++ bin/vpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # pull ANU password out of 1Password, pipe it to stdin
-op item get "ANU Identity" --fields pw |
+op read "op://Personal/ANU Identity/password" |
     # start the VPN
     sudo openconnect --user=u2548636 --protocol=gp --passwd-on-stdin https://staff-access.anu.edu.au
